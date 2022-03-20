6859f82004d86e6b94ac0af3c2985cf080d400d5
rym49 <hatanakaryoma.130@gmail.com>
2022-03-21 02:37:03 +0900

    step 2

--- book-live/src/App.tsx
+++ book-live/src/App.tsx
@@ -1,38 +1,38 @@
+import { useState } from "react";
+
 import "./App.css";
 import BookRow from "./BookRow";
-
-const dummyBooks: BookToRead[] = [
-  {
-    id: 1,
-    title: "はじめてのReact",
-    authors: "ダミー",
-    memo: ""
-  },
-  {
-    id: 2,
-    title: "React Hooks入門",
-    authors: "ダミー",
-    memo: ""
-  },
-  {
-    id: 3,
-    title: "実践Reactアプリケーション開発",
-    authors: "ダミー",
-    memo: ""
-  }
-];
+import { dummyBooks } from "./dummyData";
 
 const App = () => {
-  const bookRows = dummyBooks.map(b => {
+  const [books, setBooks] = useState(dummyBooks);
+  
+  const handleBookDelete = (id: number) => {
+    const newBooks = books.filter((b) => b.id !== id);
+    setBooks(newBooks);
+  }
+
+  const handleBookMemoChange = (id: number, memo: string) => {
+    const newBooks = books.map((b) => {
+      return b.id === id
+        ? {...b, memo: memo}
+        : b;
+    })
+
+    setBooks(newBooks);
+  }
+
+  const bookRows = books.map(b => {
     return(
       <BookRow
         book={b}
         key={b.id}
-        onMemoChange={(id, memo) => {}}
-        onDelete={(id) => {}}
+        onMemoChange={(id, memo) => handleBookMemoChange(id, memo)}
+        onDelete={(id) => handleBookDelete(id)}
       />
     );
   });
+
 
   return (
     <div className="App">
