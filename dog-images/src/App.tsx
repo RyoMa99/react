7fa14449c1bd88f0535c4148e15a0ceef1124bf9
rym49 <hatanakaryoma.130@gmail.com>
2022-03-21 17:25:11 +0900

    display dog image

--- dog-images/src/App.tsx
+++ dog-images/src/App.tsx
@@ -5,7 +5,25 @@ const App: React.VFC = () => {
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header>
+        <h1>Cute Dog Images</h1>
+      </header>
+      <main>
+        <section>
+          <figure>
+            <img
+              src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+              alt="cute dog"
+            />
+          </figure>
+        </section>
+      </main>
+      <footer>
+        <p>Dog images are retrieved from Dog API.</p>
+        <p>
+          <a href="https://dog.ceo/dog-api/about">Donate to Dog API.</a>
+        </p>
+      </footer>
     </div>
   );
 };
